90087d198f6635095d66096f3760c88df0f5723f
Beni <benitx126@gmail.com>
2021-10-16 21:31:28 +0200

    IOHandler new methods added

--- src/com/dusza/IOHandler.java
+++ src/com/dusza/IOHandler.java
@@ -4,11 +4,14 @@ package com.dusza;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class IOHandler {
         public static IOHandler single_instance = null;
         private Path mainPath;
+        private final String data_pattern = "yyyy.MM.dd HH.mm.ss";
+
 
         // constructor
         private IOHandler(Path path) {
@@ -43,25 +46,30 @@ public class IOHandler {
                     data.add(input.nextLine());
                 }
             } catch (IOException e) {
-                System.out.println("File named " + file + " not found.");
+                System.out.println("File named " + path.toString() + " not found.");
             }
 
             return data;
         }
 
         public List<String> readFiles() {
-            return readFiles(mainPath);
+            return readFiles(mainPath, false);
+        }
+        public List<String> readFiles(boolean directoriesAllowed) {
+            return readFiles(mainPath, directoriesAllowed);
         }
 
 
-        public List<String> readFiles(Path path) {
+        public List<String> readFiles(Path path, boolean directoriesAllowed) {
             List<String> data = new ArrayList<>();
             File folder = path.toFile();
 
             try {
                 DirectoryStream<Path> stream = Files.newDirectoryStream(path);
                 for (Path p : stream) {
-                    if (!Files.isDirectory(p)) {
+                    if (directoriesAllowed) {
+                        data.add(p.toString());
+                    } else if (!Files.isDirectory(p)) {
                         data.add(p.toString());
                     }
                 }
@@ -81,8 +89,58 @@ public class IOHandler {
             }
         }
 
+        public List<String> readAllCommits() {
+            Path p = mainPath.resolve("/.dusza/");
+            List<String> files = readFiles(p, true);
+            files.remove(files.size()-1); // remove head.txt
+
+            return files;
+        }
+
+        public List<String> readCommitDetails(int commitIndex) {
+            Path commitPath = getCommitPath(commitIndex);
+            return readFile(commitPath);
+        }
+
+        public Path getCommitPath(int commitIndex) {
+            return mainPath.resolve("/.dusza/" + commitIndex + ".commit/commit.details");
+        }
+
         public Commit loadCommit(int commitIndex) {
-            Commit newCommit = new Commit();
+            Commit newCommit = null;
+            Path commitPath = getCommitPath(commitIndex);
+            List<String> commitData = readFile(commitPath);
+
+
+            try(Scanner scanner = new Scanner(commitPath)) {
+                scanner.useDelimiter(" ");
+
+                scanner.next();
+                scanner.skip(scanner.delimiter());
+                String parent = scanner.nextLine();
+
+                scanner.next();
+                scanner.skip(scanner.delimiter());
+                String author = scanner.nextLine();
+
+                scanner.next();
+                scanner.skip(scanner.delimiter());
+                String date = scanner.nextLine();
+
+                scanner.next();
+                scanner.skip(scanner.delimiter());
+                String decs = scanner.nextLine();
+
+                List<String> changes = new ArrayList<>();
+                while (scanner.hasNextLine()) {
+                    changes.add(scanner.nextLine());
+                }
+
+                newCommit = new Commit(parent, author, Commit.getDateFromString(date), decs, changes);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             return newCommit;
         }
@@ -90,7 +148,6 @@ public class IOHandler {
         public HashMap<Path,ChangeType> getChanges() {
             HashMap<Path,ChangeType> changes = new HashMap<Path, ChangeType>();
             List<String> files = readFiles();
-
 
 
 
